2bf14fbc0e3ec3444a767901c5b780d90a98760a
minwu <minwu163@gmail.com>
2017-08-04 12:10:10 +0800

    update simple flaskr  without unitest !

--- flaskr.py
+++ flaskr.py
@@ -1,12 +1,84 @@
-from flask import Flask
+# -*- coding: UTF-8 -*-
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+#创建数据库模式，初始化数据化的方法： contextlib.closing() 函数
+from contextlib import closing
+import os
 
+# configuration
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+DATABASE = os.path.join(PROJECT_ROOT, 'tmp', 'flaskr.db')
+#DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# create our little application :)
 app = Flask(__name__)
-
-
+app.config.from_object(__name__)
+#app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+'''
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+'''
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+#init  database
+def init_db():
+    with closing(connect_db()) as db:
+        #数据库连接对象db
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
